26e988edbc0524226a795d104ca4a2e9120d67ff
ShashwatUpadhyay <shashwatup619@gmail.com>
2025-09-01 21:01:35 +0530

    ALLOWED_HOST tweaks

--- dynamicform/settings.py
+++ dynamicform/settings.py
@@ -29,7 +29,8 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
+ALLOWED_HOSTS = [h.strip() for h in os.getenv('ALLOWED_HOSTS', '').split(',') if h.strip()]
+
 
 if DEBUG:
     ALLOWED_HOSTS.append('*')
